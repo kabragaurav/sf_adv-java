3c18eb7ae38f5d4a77f45e38bad5c1d511ac0cd2
Saravana Kumar <saravana@Saravanas-MacBook-Pro.local>
2022-09-30 14:17:49 +0530

    jdbc examples

---
+++ labs/examples/adv-java-examples/src/com/examples/java/java11/TypeInferenceDemo.java
@@ -0,0 +1,13 @@
+package com.examples.java.java11;
+
+public class TypeInferenceDemo {
+    public static void main(String[] args) {
+        var a = 100;
+        var b = 200;
+        var c = a + b;
+        System.out.println(c);
+
+        var greetings = "Welcome to Java 11";
+        System.out.println(greetings);
+    }
+}
